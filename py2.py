ddb61ba062d58a1ca0bb60ee0c94fa0df01450a7
jeampu <49128468+jeampu@users.noreply.github.com>
2021-06-09 15:05:12 +0200

    Update py2.py

--- py2.py
+++ py2.py
@@ -1,1 +1,6 @@
 print("Hello!");
+from __future__ import print_function
+age = raw_input ("How old are you ? ")
+height = raw_input ("How tall are you ?")
+weight = raw_input ("how heavy are you ?")
+print "So you are %r years old , %r tall and %r heavy " % (age,height,weight)
